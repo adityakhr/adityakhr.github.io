7a125833d46d520a2a95d85c267186de21a38d57
Aditya Khare <aditya.khr1904@gmail.com>
2023-05-17 13:01:55 +0530

    pffinal-1

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ barContainer.addEventListener("click",function(){
 function resume(){
     window.open("https://drive.google.com/file/d/1eYN53SgKZ7bet7Z9TvNZBInMICiZH8p0/view?usp=share_link");
   }
+
+  function resume2(){
+    window.open("https://drive.google.com/file/d/1eYN53SgKZ7bet7Z9TvNZBInMICiZH8p0/view?usp=share_link");
+  }
