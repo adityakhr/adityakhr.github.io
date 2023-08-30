e4494a85604313ba1123f3693c771a73efedbd7c
Aditya Khare <aditya.khr1904@gmail.com>
2023-08-30 15:22:27 +0530

    Portfolio

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let barContainer = document.querySelector(".barContainer");
 let sidebar = document.querySelector(".links");
+let linkss = document.querySelectorAll(".links>li");
 barContainer.addEventListener("click",function(){
     barContainer.classList.toggle("active");
     sidebar.classList.toggle("active");
 });
-
+for(let i=0 ; i<linkss.length ; ++i){
+  linkss[i].addEventListener("click",function(){
+      barContainer.classList.toggle("active");
+      sidebar.classList.toggle("active");
+  });
+}
 function resume(){
   window.open("https://drive.google.com/file/d/1eYN53SgKZ7bet7Z9TvNZBInMICiZH8p0/view?usp=share_link");
 }
-function close(){
-  barContainer.classList.toggle("active");
-    sidebar.classList.toggle("active");
-}
+
+
+
+var typed = new Typed(".typing", {
+  strings: ["Java Backend Developer"],
+  typeSpeed: 60,
+  backSpeed: 20,
+  loop: true,
+});
